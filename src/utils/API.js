29f30ba4758763a2051684a0b26bef3ca917d5fd
Mark F <the.mark.fullton@gmail.com>
2020-10-22 19:05:33 -0700

    Updated utils

--- src/utils/API.js
+++ src/utils/API.js
@@ -4,13 +4,25 @@ export default {
     getPatches: function () {
         return axios.get("http://localhost:8080/patchwork/patches");
     },
-    createPatch: function () {
-        return axios.post("http://localhost:8080/patchwork/patch");
+    createPatch: function (creator, name, pattern, texture, temperment) {
+        return axios.post("http://localhost:8080/patchwork/patch", {
+            creator: creator,
+            name: name,
+            pattern: pattern,
+            texture: texture,
+            temperment: temperment
+        });
     },
     deletePatch: function (id) {
         return axios.delete(`http://localhost:8080/patchwork/patch/${id}`);
     },
-    updatePatch: function (id) {
-        return axios.put(`http://localhost:8080/patchwork/patch/${id}`);
+    updatePatch: function (id, creator, name, pattern, texture, temperment ) {
+        return axios.put(`http://localhost:8080/patchwork/patch/${id}`, {
+            creator: creator,
+            name: name,
+            pattern: pattern,
+            texture: texture,
+            temperment: temperment
+        });
     }
 }
