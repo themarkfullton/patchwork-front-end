ffdbbad0271c6ead958c4547bdc228a1e0ed2684
Mark F <the.mark.fullton@gmail.com>
2020-10-24 21:25:40 -0700

    Added setGreeting to utils

--- src/utils/API.js
+++ src/utils/API.js
@@ -24,5 +24,10 @@ export default {
             texture: texture,
             temperment: temperment
         });
+    },
+    setGreeting: function (id, greeting) {
+        return axios.put(`http://localhost:8080/patchwork/patch/${id}`, {
+            greeting: greeting,
+        });
     }
 }
