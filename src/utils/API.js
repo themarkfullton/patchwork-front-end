356adc483e80bb0aa731fd4160d6213af5e39ab1
Mark F <the.mark.fullton@gmail.com>
2020-10-27 11:27:56 -0700

    Changed API calls to new API

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 export default {
     getPatches: function () {
-        return axios.get("http://localhost:8080/patchwork/patches");
+        return axios.get("https://patchwork-api.herokuapp.com/patchwork/patches");
     },
     createPatch: function (creator, name, pattern, texture, temperment, greeting) {
-        return axios.post("http://localhost:8080/patchwork/patch", {
+        return axios.post("https://patchwork-api.herokuapp.com/patchwork/patch", {
             creator: creator,
             name: name,
             pattern: pattern,
@@ -15,10 +15,10 @@ export default {
         });
     },
     deletePatch: function (id) {
-        return axios.delete(`http://localhost:8080/patchwork/patch/${id}`);
+        return axios.delete(`https://patchwork-api.herokuapp.com/patchwork/patch/${id}`);
     },
     updatePatch: function (id, creator, name, pattern, texture, temperment, greeting ) {
-        return axios.put(`http://localhost:8080/patchwork/patch/${id}`, {
+        return axios.put(`https://patchwork-api.herokuapp.com/patchwork/patch/${id}`, {
             creator: creator,
             name: name,
             pattern: pattern,
