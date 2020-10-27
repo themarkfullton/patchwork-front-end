457b5c03d661034a1b5b56905cf42be53a280292
Mark F <the.mark.fullton@gmail.com>
2020-10-26 20:37:03 -0700

    Added intro container

---
+++ src/containers/view/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import UpdateWorkshop from "../../components/UpdateWorkshop";
+import API from "../../utils/API";
+
+class View extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            patches: [],
+            currentUpdateId: "",
+            name: "",
+            creator: "",
+            pattern: "",
+            texture: "",
+            temperment: "",
+            greeting: "",
+            updatingPatch: false,
+        };
+
+        this.toggleUpdate = this.toggleUpdate.bind(this);
+        this.deletePatch = this.deletePatch.bind(this);
+    }
+
+    componentDidMount() {
+        API.getPatches().then((res) => {
+            this.setState({ patches: res.data })
+        }).catch((err) => this.setState({ error: err.items }));
+    }
+
+    toggleUpdate(patchId) {
+        this.setState({
+            currentUpdateId: patchId
+        });
+    }
+
+    deletePatch = (patchId) => {
+        API.deletePatch(patchId).then((resp) => {
+            alert("Successfully recycled patch");
+            window.location.reload(false);
+        }).catch((err) => {
+            alert(err);
+        })
+    }
+}
